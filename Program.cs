f665c45cc981ce28b2f367b22132d21e31256809
isol04 <148771152+isol04@users.noreply.github.com>
2023-10-23 15:03:17 +0200

    Add files via upload
    (kommer ändra metoder för input)

--- Program.cs
+++ Program.cs
@@ -4,34 +4,51 @@ namespace Spelprojekt_grupp36
 {
     internal class Program
     {
-
+        
         static bool spelaresTur = true;
         static int a = 5;
         static int b = 5;
         static int c = 5;
-        static bool inGame = true;
+        static bool inGame = false;
+        static bool ProgramAktivt = true;
         static void Main(string[] args)
         {
 
-
-
             do
             {
-                
-                if (a == 0 && b == 0 && c == 0)
+
+                Console.WriteLine(regler());
+                Console.WriteLine(" \r\nskriv start för att starta spelet eller stop för att stänga av programmet!");
+                string input = Console.ReadLine();
+                if  (input == "start")
                 {
-                    Vinnare(!spelaresTur);
+                    inGame = true;
                 }
+                if (input == "stop")
+                {
+                    ProgramAktivt = false;
+                }
+                while (inGame) 
+                {
 
-                Uppdatera(a, b, c);
-                Drag(spelaresTur, a, b, c);
+
+                    if (a == 0 && b == 0 && c == 0)
+                    {
+                        Vinnare(!spelaresTur);
+
+                    }
+                    else
+                    {
+                        Uppdatera(a, b, c);
+                        Drag(spelaresTur, a, b, c);
+                    }
 
 
+                }
+               
 
             }
-            while (inGame);
-
-
+            while (ProgramAktivt);
         }
 
         static void Vinnare(bool spelare)
@@ -42,17 +59,34 @@ namespace Spelprojekt_grupp36
             a = 5;
             b = 5;
             c = 5;
+
+            //nollställer spelet
             
         }
+
+        static string regler()
+        {
+
+            string regler = "Välkommen till spelet Nim! \n\n\n\n Nims regler lyder följande: \n 1. Spelet börjar med att 15 pinnar placeras i tre olika lika stora högar. \n 2. Därefter väljer startspelaren först en hög och sedan eliminerar spelaren x antal pinnar från den högen (minst en pinne). \n 3. Därefter gör nästa spelare sak. \n 4. Spelarna turas sedan om till att endast en pinne kvarstår. \n 5. Spelaren som väljer den sista pinnen vinner då spelet.";
+
+            return regler;
+
+
+        }
+
+
+
+
+
         static void Drag(bool spelare, int hög1, int hög2, int hög3)
         {
-            if (spelare == true)
+            if (spelare == true) //avgör vems tur det är
             {
                 Console.WriteLine("Det är spelare 1s tur");
             }
             else Console.WriteLine("Det är spelare 2s tur");
 
-            try
+            try //denna try-catch kollar ifall högen man tar ifrån finns och 
             {
                 Console.WriteLine("Ange vilken hög du vill ta från");
                 int drag = int.Parse(Console.ReadLine());
@@ -153,10 +187,7 @@ namespace Spelprojekt_grupp36
             }
 
         }
-        static void Pausa()
-        {
-            inGame = false;
-        }
+        
         static void Uppdatera(int hög1, int hög2, int hög3) //ritar upp pinnar
         {
             for (int i = 0; i < hög1;  i++)
